343d55f7ffcd8eb03ce02aba9156db425292ef76
Aaron Rush <arush1@live.co.uk>
2024-03-12 20:29:11 +0000

    Use secureRandom for token generation

--- src/main/java/com/rushtech/urlshortener/util/TokenGenerator.java
+++ src/main/java/com/rushtech/urlshortener/util/TokenGenerator.java
@@ -1,15 +1,21 @@
 package com.rushtech.urlshortener.util;
 
-import java.util.UUID;
+import java.security.SecureRandom;
 
 public class TokenGenerator implements ITokenGenerator {
 
     private static final int CODE_LENGTH = 6;
+    private static final int RANDOM_INTEGER_RANGE = 16;
+
+    private static final SecureRandom secureRandom = new SecureRandom();
 
     @Override
     public String generateToken() {
-        String uuid = UUID.randomUUID().toString();
-        uuid = uuid.replaceAll("-", "");
-        return uuid.substring(0, CODE_LENGTH);
+        StringBuilder token = new StringBuilder(CODE_LENGTH);
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            int randomValue = secureRandom.nextInt(RANDOM_INTEGER_RANGE); // Generate a random integer between 0 and RANDOM_INTEGER_RANGE (inclusive)
+            token.append(Integer.toHexString(randomValue)); // Convert the integer to a hexadecimal character
+        }
+        return token.toString();
     }
 }
