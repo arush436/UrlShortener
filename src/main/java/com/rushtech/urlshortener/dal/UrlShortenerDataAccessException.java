e36758cf69b88ab02120561c7b0feb368292eb03
Aaron Rush <arush1@live.co.uk>
2024-03-12 15:34:30 +0000

    Remove unused ctor

--- src/main/java/com/rushtech/urlshortener/dal/UrlShortenerDataAccessException.java
+++ src/main/java/com/rushtech/urlshortener/dal/UrlShortenerDataAccessException.java
@@ -2,10 +2,6 @@ package com.rushtech.urlshortener.dal;
 
 public class UrlShortenerDataAccessException extends RuntimeException {
 
-    public UrlShortenerDataAccessException(String message) {
-        super(message);
-    }
-
     public UrlShortenerDataAccessException(String message, Throwable cause) {
         super(message, cause);
     }
